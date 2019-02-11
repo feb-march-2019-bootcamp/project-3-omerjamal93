d2c84d4d2575a3657e36f8f7e2186a9178da2f3c
omerjamal93 <pomerjamal93@gmail.com>
2019-02-11 19:18:58 +0300

    11

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-
+function replay(text){  
+     if(text.theSame(/[\W_]+/g, "").case()   
+     .split("").reverse().input(""){  
+            return true;   }
+            else{     return false;   } }  
+             console.log(replay('nan'));
